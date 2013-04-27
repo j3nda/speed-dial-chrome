1f969652d88fe54e76aa40a9218e717dd7f374fa
Matt Ellis <m.ellis27@gmail.com>
2013-04-27 17:14:44 +1000

    Combined bookmarks.js back into newtab.js as the functoinality couldn't be sufficiently isolated

--- javascript/newtab.js
+++ javascript/newtab.js
@@ -2,6 +2,24 @@
 * Generation and maintenence of the speed dial interface
 */
 
+// Adds a new bookmark to chrome, and displays it on the speed dial
+function addBookmark(title, url) {
+	if (title.length === 0 || url.length === 0)
+		return;
+
+	// Chrome won't create bookmarks without HTTP
+	if (url.indexOf("http") !== 0)
+		url = 'http://' + url;
+
+	var folder_id  = localStorage['folder'];
+	var dfolder_id = localStorage['default_folder_id'];
+
+	if (dfolder_id !== undefined || dfolder_id > 1) {
+		folder_id = dfolder_id;
+	}
+
+	chrome.bookmarks.create({'parentId': folder_id, 'title': title, 'url': url}, function() {});
+}
 
 // Adds a bookmark onto the speed dial. Takes a chrome bookmark node object.
 function addSpeedDialEntry(bookmark) {
@@ -150,6 +168,13 @@ function getEntryHtml(bookmark) {
 					'</div>';
 
 	return entryHtml;
+}
+
+// Deletes a bookmarks and removes it from the speed dial
+function removeBookmark(id) {
+	chrome.bookmarks.remove(id, function() {
+		removeSpeedDialEntry(id);
+	});
 }
 
 // Removes a bookmark entry from the speed dial
